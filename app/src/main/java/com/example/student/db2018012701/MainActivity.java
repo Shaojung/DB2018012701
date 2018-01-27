5ead7e89818318580f00a6c7fe4434b77617b763
Shaojung <shaojung@gmail.com>
2018-01-27 09:50:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2018012701/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.student.db2018012701;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("這是對話框");
+        builder.setMessage("這是內容訊息");
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "按下確定", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+    }
+}
